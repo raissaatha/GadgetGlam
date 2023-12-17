bd0bdf7fba04176eb406f7af82ea86a606dba83f
raissaatha <raissaatha01@gmail.com>
2023-12-17 21:25:44 +0700

    update app.blade.php

--- resources/views/frontend/layout/app.blade.php
+++ resources/views/frontend/layout/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="dicoding:email" content="21081010244@student.upnjatim.ac.id">
     <title>GADGET GLAM - Menjual Peralatan Komputer</title>
 
     <link rel="stylesheet" href="{{ asset('assets/css/style3.css') }}">
